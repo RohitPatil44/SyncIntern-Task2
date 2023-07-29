39fda1ec151085162f87fb3210bb239dcdcc649a
Rohit Patil <rohitbpatil991@gmail.com>
2023-07-29 18:20:05 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 3,
+  question: "Who is Father of JAVA?",
+  answer: "James Gosling",
+  options: [
+    "James Gosling",
+    "Dennis Ritchie",
+    "Bjarne Stroustrup",
+    "Guido van Rossum"
+  ]
+},
+  {
+  numb: 4,
+  question: "Who is the founder of SyncInterns?",
+  answer: "Naveen Kumar",
+  options: [
+    "Sundar Pichai",
+    "Naveen Kumar",
+    "Reid Hoffman",
+    "Sandeep Jain"
+  ]
+},
+  {
+  numb: 5,
+  question: "Head Office of Google is suited in which city",
+  answer: "Mountain View, California",
+  options: [
+    "London, England",
+    "New Delhi, India",
+    "New York, USA",
+    "Mountain View, California"
+  ]
+},
+
+];
